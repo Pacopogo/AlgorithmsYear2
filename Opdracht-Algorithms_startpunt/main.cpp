eed569774e043876b11ec3aa26619d43a02eef24
Pablo Jacobs <pablo.jacobs.02@gmail.com>
2025-10-15 13:05:51 +0200

    StartPunt

---
+++ Opdracht-Algorithms_startpunt/main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+
+int main() {
+	// gebruik functies uit <algorithm> en <functional> om de volgende opdrachten uit te voeren:
+
+	// splits de vector in 2 nieuwe vectoren: 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+	{
+		std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
+
+
+	}
+
+	// maak alle elementen UPPERCASE
+	{
+		std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
+	}
+
+	// verwijder alle dubbele elementen
+	{
+		std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
+	}
+
+	// verwijder alle negatieve elementen
+	{
+		std::vector<double> numbers{ 10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635 };
+
+		auto it = max_element(numbers.begin(), numbers.end());
+
+		std::cout << *it;
+		std::cout << "\n";
+
+	}
+
+	// bepaal voor alle elementen of ze even of oneven zijn
+	{
+		std::vector numbers{ 10, 324422, 6, -23, 234, 654, 3, -9, 635 };
+	}
+
+	// bepaal de som, het gemiddelde, en het product van alle getallen te berekenen
+	{
+		std::vector<double> numbers{ 10, 324422.1, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635 };
+	}
+
+
+	std::cout << "\ntest";
+
+	return 0;
+}
